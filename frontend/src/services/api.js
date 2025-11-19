b900a17e3f180e70620233d32d3873f8d75dbdaa
AZMEERASAI <152939740+AZMEERASAI@users.noreply.github.com>
2025-11-19 10:46:58 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,7 @@
 
 // src/services/api.js
 import axios from 'axios';
-
-<<<<<<< HEAD
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-=======
 const API = axios.create({ baseURL: 'http://localhost:31034/api' });
->>>>>>> e8be3ddfcdd9993b7c45e4c4fe9c0f77ccff7b74
-
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
@@ -16,3 +10,4 @@ API.interceptors.request.use(config => {
 
 
 export default API;
+
